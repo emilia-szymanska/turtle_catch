065e052c8d1c3db3c318dfc1a6c7e4b8e4c9e5f9
emilia-szymanska <emiliaszym@gmail.com>
2020-11-05 00:00:48 +0100

    Add chasing functionality

--- src/turtle_catch/src/turtle_catcher.py
+++ src/turtle_catch/src/turtle_catcher.py
@@ -2,13 +2,14 @@
 
 import rospy
 from turtlesim.msg import Pose
-from geometry_msgs.msg import Twist
-from math import atan2, pi
+from geometry_msgs.msg import Twist, Point
+from math import atan2, pi, sqrt
 from numpy import sign
 
 class TurtleCatcher:
     def __init__(self):
         self.vel = rospy.Publisher('cmd_vel', Twist, queue_size = 1)
+        self.dist = rospy.Publisher('turtles_distance', Point, queue_size = 1)
         
         self.cmd_vel = Twist()
         self.cmd_vel.linear.x = 0
@@ -39,44 +40,58 @@ class TurtleCatcher:
         
 
     def catching_callback(self, msg_received):
-#        rospy.loginfo("Received something: x = " + str(msg_received.x) + " y = " + str(msg_received.y))
         x = msg_received.x
         y = msg_received.y
 
         new_theta = self.get_ang_distance(x, y)
-        
+        dist = self.get_distance(x, y)
+
         msg = Twist()
-        msg.linear.x = self.cmd_vel.linear.x
         msg.linear.y = self.cmd_vel.linear.y
         msg.linear.z = self.cmd_vel.linear.z
 
         msg.angular.x = self.cmd_vel.angular.x
         msg.angular.y = self.cmd_vel.angular.y
        
-        alfa = self.pose.theta
-        beta = new_theta
-
-        if alfa * beta > 0:
-            ang_diff = beta - alfa
-        elif abs(alfa) + abs(beta) < pi:
-            ang_diff = sign(beta) * (abs(alfa) + abs(beta))
+        
+        if dist < 0.5:
+            msg.angular.z = 0
+            msg.linear.x = 0
         else:
-            ang_diff = sign(beta) * (-2 * pi + (abs(alfa) + abs(beta)))
-            
-        msg.angular.z = ang_diff
-        self.cmd_vel.angular.z = ang_diff
+            alpha = self.pose.theta
+            beta = new_theta
 
+            if alpha * beta > 0:
+                ang_diff = beta - alpha
+            elif abs(alpha) + abs(beta) < pi:
+                ang_diff = sign(beta) * (abs(alpha) + abs(beta))
+            else:
+                ang_diff = sign(beta) * (-2 * pi + (abs(alpha) + abs(beta)))
+            
+            msg.angular.z = ang_diff
+            self.cmd_vel.angular.z = ang_diff
+            
+            msg.linear.x = 1
+            self.cmd_vel.linear.x = 1
+        
+        dist_msg = Point()
+        dist_msg.x = x - self.pose.x
+        dist_msg.y = y - self.pose.y
+        dist_msg.z = 0
+
+        self.dist.publish(dist_msg)
         self.vel.publish(msg)        
 
-        rospy.loginfo("Theta: " + str(new_theta))
 
     def get_distance(self, goal_x, goal_y):
         distance = sqrt(pow((goal_x - self.pose.x), 2) + pow((goal_y - self.pose.y), 2)) 
         return distance
 
+
     def get_ang_distance(self, goal_x, goal_y):
-        ang_distance = atan2(goal_y - self.pose.y, goal_x - self.pose.x) #- self.pose.theta) 
+        ang_distance = atan2(goal_y - self.pose.y, goal_x - self.pose.x) 
         return ang_distance
+
 
     def run(self):
         while not rospy.is_shutdown():
